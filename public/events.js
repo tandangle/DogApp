abb6555b38d630f0a7525d9fb2df1b7229e4c78c
Tan Le-Dang <tledang@rsglab.com>
2020-05-11 03:46:23 -0400

    working on rendering latest timestamps on dashboard

--- public/events.js
+++ public/events.js
@@ -1,3 +1,11 @@
+flatpickr(".flatpickr", 
+{
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    defaultDate: new Date(),
+    maxDate: new Date()
+});
+
 function food() {
     var elem1 = document.getElementById("events");
     var elem2 = document.getElementById("events2container")
@@ -106,6 +114,64 @@ function foodWetBack() {
 }
 
 document.getElementById("food_wet_arrow").addEventListener("click", foodWetBack);
+
+//
+
+function wetfoodDate() {
+  var elem1 = document.getElementById("events_food_wet_container");
+  var elem2 = document.getElementById("events_food_wet_date_container")
+  var pos = 0;
+  var id = setInterval(frame, .25);
+  function frame() {
+    if (pos == -2000) {
+      clearInterval(id);
+    } else {
+      pos = pos - 10;
+      elem1.style.left = pos + 'px';
+    }
+  }
+  var pos2 = 2000;
+  var id2 = setInterval(frame2, .25);
+  function frame2() {
+      if (pos2 == 0) {
+          clearInterval(id2)
+      } else {
+          pos2 = pos2 - 10;
+          elem2.style.left = pos2 +"px";
+      } 
+  }
+}
+
+document.getElementById("wetfoodate").addEventListener("click", wetfoodDate);
+
+function foodWetDateBack() {
+  var elem1 = document.getElementById("events_food_wet_container");
+  var elem2 = document.getElementById("events_food_wet_date_container")
+  var pos = -2000;
+  var id = setInterval(frame, .25);
+  function frame() {
+    if (pos == 0) {
+      clearInterval(id);
+    } else {
+      pos = pos + 10;
+      elem1.style.left = pos + 'px';
+    }
+  }
+  var pos2 = 0;
+  var id2 = setInterval(frame2, .25);
+  function frame2() {
+      if (pos2 == 2000) {
+          clearInterval(id2)
+      } else {
+          pos2 = pos2 + 10;
+          elem2.style.left = pos2 +"px";
+      } 
+  }
+}
+
+document.getElementById("food_wet_date_arrow").addEventListener("click", foodWetDateBack);
+
+//
 
 
 document.getElementById("wet_now").addEventListener("click", function(){
