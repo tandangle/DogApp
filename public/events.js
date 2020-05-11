dc39502e95a141ae32c7cc95c19828dc98fdd0f2
Tan Le-Dang <tledang@rsglab.com>
2020-05-11 00:49:52 -0400

    poop works, next step adding specific time pickers for food and potty

--- public/events.js
+++ public/events.js
@@ -284,3 +284,63 @@ document.getElementById("events_potty_pee_arrow").addEventListener("click", peeB
 document.getElementById("pee_now").addEventListener("click", function(){
   document.getElementById("form_pee_now").submit()
 });
+
+//
+
+function poop() {
+  var elem1 = document.getElementById("events_potty_container");
+  var elem2 = document.getElementById("events_potty_container_poo")
+  var pos = 0;
+  var id = setInterval(frame, .25);
+  function frame() {
+    if (pos == -2000) {
+      clearInterval(id);
+    } else {
+      pos = pos - 10;
+      elem1.style.left = pos + 'px';
+    }
+  }
+  var pos2 = 2000;
+  var id2 = setInterval(frame2, .25);
+  function frame2() {
+      if (pos2 == 0) {
+          clearInterval(id2)
+      } else {
+          pos2 = pos2 - 10;
+          elem2.style.left = pos2 +"px";
+      } 
+  }
+}
+
+document.getElementById("poop").addEventListener("click", poop);
+
+function poopBack() {
+  var elem1 = document.getElementById("events_potty_container");
+  var elem2 = document.getElementById("events_potty_container_poo");
+  var pos = -2000;
+  var id = setInterval(frame, .25);
+  function frame() {
+    if (pos == 0) {
+      clearInterval(id);
+    } else {
+      pos = pos + 10;
+      elem1.style.left = pos + 'px';
+    }
+  }
+  var pos2 = 0;
+  var id2 = setInterval(frame2, .25);
+  function frame2() {
+      if (pos2 == 2000) {
+          clearInterval(id2)
+      } else {
+          pos2 = pos2 + 10;
+          elem2.style.left = pos2 +"px";
+      } 
+  }
+}
+
+document.getElementById("events_potty_poo_arrow").addEventListener("click", poopBack);
+
+document.getElementById("poo_now").addEventListener("click", function(){
+  document.getElementById("form_poo_now").submit()
+});
